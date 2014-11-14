620e34a4b2bcfd8b725ff5e6b4e7147be876c045
Melissa Xie <me@melissaxie.com>
2014-11-14 15:37:23 -0500

    Update Vundle config to use its new interface

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ bash <(curl -s https://raw.githubusercontent.com/thoughtbot/laptop/master/mac) 2
 echo 'Using RCM to symlink dotfiles'
 rcup -v
 
-vundle_dir=~/.vim/bundle/vundle
-if [ ! -d $vundle_dir ]; then
+vundle_plugin=~/.vim/bundle/Vundle.vim
+if [ ! -d $vundle_plugin ]; then
   echo 'Installing vundle'
-  git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+  git clone https://github.com/gmarik/Vundle.vim.git $vundle_plugin
 fi
 
-echo 'Running BundleInstall'
-vim -u vim/vundle.vim +BundleInstall +qa
+echo 'Running vundle'
+vim -u vim/vundle.vim +PluginInstall +qa
 
 # restart the shell
 exec -l $SHELL
